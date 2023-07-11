34f3467b2dbbdb7b28565c8bcf5266a978c489c4
Tess <tess.merrill25@gmail.com>
2023-07-11 16:23:20 -0600

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block >= 42) {
+        return block - 42;
+    } else if (block < 42) {
+        return 42 - block;
+    }
+};
+
+function distanceFromHqInFeet(block) {
+   return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        return (start-destination)*264;
+    } else if (start < destination) {
+        return (destination-start)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination)-400)*0.02;
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) >2500) {
+        return "cannot travel that far";
+    }
+}
